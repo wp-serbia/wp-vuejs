1bea1f1309e066a2ab850d1246782f4eaa722e26
momir.despotovic <momir.despotovic@nettvplus.org>
2019-08-18 20:34:38 +0200

    Config changes

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,9 @@
 <template>
   <div id="app">
-
-    <TopBar
-      @showUpdater="showUpdater = true"
-    />
+    <TopBar @showUpdater="showUpdater = true" />
 
     <div id="wrapper">
-
-      <Updater
-        v-if="showUpdater"
-        :error="error"
-        @close="showUpdater = false"
-      />
+      <Updater v-if="showUpdater" :error="error" @close="showUpdater = false" />
 
       <Loading
         v-if="loadingOn"
@@ -20,7 +12,6 @@
       />
 
       <router-view :key="this.viewKey"></router-view>
-
     </div>
 
     <Foot />
@@ -28,93 +19,91 @@
 </template>
 
 <script>
-  import bus from './bus';
-  import Foot from './components/Foot';
-  import Loading from './components/Loading';
-  import Updater from './components/Updater';
-  import TopBar from './components/TopBar';
+import bus from "./bus";
+import Foot from "./components/Foot";
+import Loading from "./components/Loading";
+import Updater from "./components/Updater";
+import TopBar from "./components/TopBar";
 
-  export default {
-    name: 'App',
+export default {
+  name: "App",
 
-    data () {
-      return {
-        error: '',
-        loadingOn: true,
-        loadingMessage: 'Loading WP Vue',
-        loadingWheel: true,
-        showUpdater: false,
-        viewKey: 0,
+  data() {
+    return {
+      error: "",
+      loadingOn: true,
+      loadingMessage: "Loading WordPress Vue",
+      loadingWheel: true,
+      showUpdater: false,
+      viewKey: 0
+    };
+  },
+
+  created: function() {
+    this.assignEndpointFromURL();
+
+    bus.$on("toggleLoading", (status = true) => {
+      if (typeof status === "string") {
+        this.loadingOn = true;
+        this.showWheel = false;
+        this.loadingMessage = status;
+        return;
       }
-    },
 
-    created: function () {
+      this.loadingMessage = "Loading...";
+      this.loadingWheel = true;
+      this.loadingOn = status;
+    });
 
-      this.assignEndpointFromURL();
+    bus.$on("bumpViewKey", loadingMessage => {
+      bus.$emit("toggleLoading", loadingMessage);
+      this.viewKey = this.viewKey + 1;
+    });
 
-      bus.$on('toggleLoading', (status = true) => {
+    bus.$on("showUpdater", errorMessage => {
+      this.showUpdater = true;
+      this.error = errorMessage;
+    });
 
-        if(typeof status === 'string') {
-          this.loadingOn = true;
-          this.showWheel = false;
-          this.loadingMessage = status;
-          return;
-        }
+    bus.$on("clearError", () => {
+      this.error = "";
+    });
+  },
 
-        this.loadingMessage = 'Loading...';
-        this.loadingWheel = true;
-        this.loadingOn = status;
-      });
+  methods: {
+    assignEndpointFromURL: function() {
+      let endpoint = this.getQueryString("endpoint");
 
-      bus.$on('bumpViewKey', (loadingMessage) => {
-        bus.$emit('toggleLoading', loadingMessage);
-        this.viewKey = this.viewKey + 1;
-      });
+      if (!endpoint) return;
 
-      bus.$on('showUpdater', (errorMessage) => {
-        this.showUpdater = true;
-        this.error = errorMessage;
-      });
-
-      bus.$on('clearError', () => {
-        this.error = '';
-      });
-    },
-
-    methods: {
-      assignEndpointFromURL: function () {
-        let endpoint = this.getQueryString('endpoint');
-
-        if(!endpoint) return;
-
-        this.$store.commit('updateEndpoint', endpoint);
-      }
-    },
-
-    components: {
-      Foot,
-      Loading,
-      Updater,
-      TopBar
+      this.$store.commit("updateEndpoint", endpoint);
     }
+  },
+
+  components: {
+    Foot,
+    Loading,
+    Updater,
+    TopBar
   }
+};
 </script>
 
 <style lang="scss">
-  @import "assets/scss/_base.scss";
+@import "assets/scss/_base.scss";
 
-  #app {
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+#wrapper {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 1rem;
+
+  @include media($small) {
+    padding: 2rem;
   }
-
-  #wrapper {
-    max-width: 1200px;
-    margin: 0 auto;
-    padding: 1rem;
-
-    @include media($small) {
-      padding: 2rem;
-    }
-  }
+}
 </style>
