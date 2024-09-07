69a00b9c735d4c0deea2c607bd8d25eb3796e4fe
Momir Despotovic <despotovic.m@gmail.com>
2024-09-07 20:25:21 +0200

    Updtates

--- src/components/Foot.vue
+++ src/components/Foot.vue
@@ -1,23 +1,23 @@
 <template>
   <footer>
-    &copy; 2019 <a href="https://www.joomlaserbia.info">Joomla Serbia</a>.
+    &copy; 2024 <a href="https://www.joomlaserbia.info">Joomla Serbia</a>.
   </footer>
 </template>
 
 <script>
-  export default {
-    name: 'Foot'
-  }
+export default {
+  name: "Foot"
+};
 </script>
 
 <style scoped lang="scss">
-  footer {
-    text-align: center;
-    padding: 1rem;
-    background: $gray--light;
-  }
+footer {
+  text-align: center;
+  padding: 1rem;
+  background: $gray--light;
+}
 
-  a {
-    font-weight: bold;
-  }
+a {
+  font-weight: bold;
+}
 </style>
